e3ca243113d9e8430ad8ceb7e7894fba6bd3ed53
webtel <80962225+meael0016@users.noreply.github.com>
2021-04-17 19:49:17 -0400

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,37 +1,93 @@
+//ancho de la pantaya total
+var ancho= window.innerWidth;
+(function hola() {
+	
+	
+	var ancho= window.innerWidth;
+	console.log(ancho);
+	
+	
+		
 
-$("#como").click(function () {
-	$("#h1,#h2,#h3").slideToggle();	
+	if (ancho <= 981) {
+		var despegable=document.getElementById("despegable");
+		var menu=document.getElementById("menu");
+		menu.style.display="none";
+		despegable.style.display="none";
+		despegable.style.flexDirection="column";
+	}
+	
+
+	
+	
+
+	$("#nuestros_productos").mouseover(function () {
+		if (ancho > 981) {
+			$("#despegable").slideDown(30);	
+			} 
+		});
+
+			$("#nuestros_productos").mouseleave(function() {
+				if (ancho > 981) {
+				$("#despegable").slideUp(30);
+					
+				}
+			});
+			if (ancho > 981) {
+				var menu=document.getElementById("menu");
+				menu.style.display="flex"
+				var despegable=document.getElementById("despegable");
+				despegable.style.display="none";
+				despegable.style.flexDirection="column";
+			}
+		
+
+	
+		
+	
+	
+
+	window.onresize =hola;		
+	
+})();
+ $("#quienes_somos,#hola1").click(function(){
+	 $("#modal").slideDown(100);	
+	 	});
+	 
+$("#conte1").click(function(){
+	$("#modal").slideUp(100);	
+
+	 });
+
+$("#menu1").click(function(){
+		 $("#menu").slideDown(100);	
+
+	 });
+$("#fa-times").click(function(){
+	$("#menu").slideUp(100);	
+});
+$("#gmail1").click(function() {
+	$("#form").slideDown(100);
 });
 
-
-function reportWindowSize() {
-  
-  var ancho= window.innerWidth;
-  if (ancho  > 800) {
-	
-	var esto=document.getElementById("h1");
-	var esto2=document.getElementById("h2");
-	var esto3=document.getElementById("h3");
-
-		esto.style.display="flex";
-		esto2.style.display="flex";
-		esto3.style.display="flex";
-
-}
-	if (ancho  <= 800) {
-	
-	var esto=document.getElementById("h1");
-	var esto2=document.getElementById("h2");
-	var esto3=document.getElementById("h3");
-		esto.style.display="none";
-		esto2.style.display="none";
-		esto3.style.display="none";
-}
-}
+$("#quit1").click(function() {
+	$("#form").slideUp(100);
+});
+function pulso() {
+			
+		
+		if(ancho <= 981){
+			$("#despegable").slideToggle(30);
+			}
+		}
 
 
-window.onresize = reportWindowSize;
+//ancho del despegable
+
+
 
 
 
 	
+ 
+

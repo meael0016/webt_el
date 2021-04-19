c33313cf42dad4497123c6018e5afcfdf415166e
webtel <80962225+meael0016@users.noreply.github.com>
2021-04-19 10:56:10 -0400

    Add files via upload

--- php.php
+++ php.php
@@ -1,21 +1,42 @@
-<?php
-	if (isset($POST["boton"])) {
-		if (!empty($POST["nombre"]) && !empty($POST["gmail"]) && !empty($POST["asunto"]) && !empty($POST["mensaje"])) {
-			$name=$POST["nombre"];
-			$gmail=$POST["gmail"];
-			$asunto=$POST["asunto"];
-			$mensaje=$POST["mensaje"];
-			$header="From: meaelsilva@gmail.com";
-			$header.="Reply-to: meaelsilva@gmail.com ";
-			$header.="X.Mailer= PHP/". phpversion();
-			$mail=mail($gmail,$asunto,$mensaje,$header);
-			if ($mail) {
-				echo "se mando";
-			}else{
-				echo "no se mando";
-			}
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Enviar mensaje GMAIL</title>
+	<link rel='stylesheet' type='text/css' href='css/formulario.css'>
+</head>
+<body>
+		<form method="post" id="form" >
+			<div class="formulario">
+				<img src="img/logo.JPEG">
+				<input type="text" name="nombre" id="nombre" placeholder="Nombre">
+				<input type="gmail" name="gmail" id="gmail" placeholder="Gmail">
+				<input type="text" name="asunto" id="asunto" placeholder="Asunto">
+				<textarea name="mensaje" id="mensaje" placeholder="Mensaje"></textarea>
+				<input type="submit" name="boton" id="boton">
+			</div>
+		</form>
+</body>
+</html>
 
+<?php
+	if (isset($_POST["boton"])) {
+		if (!empty($_POST["nombre"]) && !empty($_POST["gmail"]) && !empty($_POST["asunto"]) && !empty($_POST["mensaje"])) {
+
+			$name=$_POST["nombre"];
+			$gmail=$_POST["gmail"];
+			$mensaje=$_POST["mensaje"];
+			$asunto=$_POST["asunto"];
+			$header="From: juanxita022@gmail.com"."\r\n";
+			$header.="Reply-To: juanxita022@gmail.com"."\r\n";
+			$header.="X-Mailer: PHP/". phpversion();
+			$mail=@mail("meaelsilva@gmail.com",$asunto,$mensaje,$header);
+			if ($mail) {
+				echo "<h1>enviado corestamente</h1";
+			}else{
+				echo "<h1>no enviado</h1";
+			}
 		}
 		
 	}
+
 ?>

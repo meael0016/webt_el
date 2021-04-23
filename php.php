c3be90b13d7274e38c50187198b7328d64985627
webtel <80962225+meael0016@users.noreply.github.com>
2021-04-23 14:33:37 -0400

    Add files via upload

--- php.php
+++ php.php
@@ -5,15 +5,11 @@
 			$asunto=$_POST["asunto"];
 			$mensaje=$_POST["mensaje"];
 			$header="From: meaelsilva@gmail.com";
-			$header.="Reply-To: meaelsilva@gmail.com ";
+			$header.="Reply-To: meaelsilva@gmail.com";
 			$header.="X.Mailer= PHP/". phpversion();
 			$mail=mail($gmail,$asunto,$mensaje,$header);
 		}
 		if ($mail) {
 			echo "<h3>sea enviado</h3";
-		}else{
-			echo "<h3>no sea enviado</h3>";
 		}
-		
 	}
-?>
